7c5a23b082638c1541b4f30102c06f96fc771a57
Chace Hartwell <chace.l.hartwell@icloud.com>
2018-08-28 15:29:59 -0600

    initialize

---
+++ src/components/Game.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="game">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Game",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/store.js
+++ src/store.js
@@ -1,11 +1,17 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Axios from 'axios';
 
 Vue.use(Vuex)
 
+let gameApi = Axios.create({
+  baseURL: 'https://inspire-server.herokuapp.com/cards',
+  timeout: 3000
+})
+
 export default new Vuex.Store({
   state: {
-
+    cards: []
   },
   mutations: {
 
